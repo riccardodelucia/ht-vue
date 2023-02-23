c26770d0d9beb3d7d037fec6901af3e7d8cfb42a
riccardo.delucia <riccardo.delucia@fht.org>
2023-02-23 22:26:08 +0100

    test

--- vite.config.js
+++ vite.config.js
@@ -37,7 +37,7 @@ export default defineConfig({
           'vue-feather': 'VueFeather',
           vuex: 'Vuex',
           'vue-router': 'VueRouter',
-          keycloak: 'Keycloak',
+          'keycloak-js': 'Keycloak',
           tippy: 'tippy',
           xstate: 'XState',
           lodash: '_',
