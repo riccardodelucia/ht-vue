7b39b02282f8073bc334da09f89d481a26cb4b6f
riccardo.delucia <riccardo.delucia@fht.org>
2023-11-20 17:27:58 +0100

    app launched from npm run dev

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <p>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolor, quos
+      error voluptas sit similique libero eaque officia, excepturi distinctio
+      cum, beatae soluta saepe. Assumenda suscipit natus, sunt illum totam
+      expedita.
+    </p>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
---
+++ src/main.js
@@ -0,0 +1,12 @@
+import { createApp } from 'vue';
+
+import '@computational-biology-sw-web-dev-unit/ht-design/style.css';
+
+import VueFeather from 'vue-feather';
+
+import App from './App.vue';
+
+const app = createApp(App); //.use(HTVue);
+app.component(VueFeather.name, VueFeather);
+
+app.mount('#app');
