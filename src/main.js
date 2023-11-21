3080e25d425f764bab245ee2c88c00423b934f65
Riccardo Roberto De Lucia <riccardo.delucia@fht.org>
2023-11-21 12:47:19 +0100

    Resolve "Add testing page for components and search bar hints"

---
+++ src/main.js
@@ -0,0 +1,14 @@
+import { createApp } from 'vue';
+
+import '@computational-biology-sw-web-dev-unit/ht-design/style.css';
+
+import VueFeather from 'vue-feather';
+
+import { HTVue } from './plugin.js';
+
+import App from './App.vue';
+
+const app = createApp(App).use(HTVue);
+app.component(VueFeather.name, VueFeather);
+
+app.mount('#app');
