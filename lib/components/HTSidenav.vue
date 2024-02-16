6fad1976105d8222baf81708ba3f86520be60c75
riccardo.delucia <riccardo.delucia@fht.org>
2024-02-16 15:56:15 +0100

    app layout injectable slot sidenav content

--- lib/components/HTSidenav.vue
+++ lib/components/HTSidenav.vue
@@ -3,32 +3,21 @@
     <a class="logo" href="https://humantechnopole.it/en/" target="_blank"
       ><img :src="htLogoWhite" alt="HT logo"
     /></a>
-    <h2>{{ sidenavObject.title }}</h2>
+    <h2>{{ title }}</h2>
     <ul class="ht-reset sidenav-list">
-      <li v-for="link in sidenavObject.links" :key="link.id">
-        <RouterLink active-class="active" :to="link.url">
-          <VueFeather :type="link.type || 'chevron-right'"></VueFeather
-          ><span>{{ link.label }}</span>
-        </RouterLink>
-      </li>
+      <slot name="sidenav-list"></slot>
     </ul>
   </aside>
 </template>
 
 <script>
-import VueFeather from 'vue-feather';
-import { RouterLink } from 'vue-router';
-
 import htLogoWhite from '../assets/ht-logo-white.svg';
 
 export default {
   name: 'HTSidenav',
-  components: { VueFeather, RouterLink },
+
   props: {
-    sidenavObject: {
-      type: Object,
-      default: () => ({}),
-    },
+    title: { type: String, default: 'Test' },
   },
   setup() {
     return {
@@ -63,38 +52,5 @@ export default {
 
 .sidenav-list {
   list-style: none;
-
-  li {
-    > a {
-      display: flex;
-      align-items: center;
-      gap: var(--size-3);
-      padding: var(--size-2);
-      margin-bottom: var(--size-1);
-      cursor: pointer;
-
-      text-decoration: none;
-      color: white;
-
-      &:not(&.active):hover,
-      &:not(&.active):focus,
-      &.active {
-        background-color: var(--ht-color-secondary);
-
-        span {
-          transform: translateX(0.5em);
-          transition: transform 0.3s ease-in-out;
-        }
-      }
-
-      > * {
-        flex-shrink: 0;
-      }
-
-      span {
-        transition: transform 0.3s ease-in-out;
-      }
-    }
-  }
 }
 </style>
