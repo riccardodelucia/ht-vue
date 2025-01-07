adb08f5cdb8edec22db180a5c9e918709f525228
riccardo.delucia <riccardo.delucia@fht.org>
2025-01-07 21:09:06 +0100

    selected ok

--- src/components/HTSelect.vue
+++ src/components/HTSelect.vue
@@ -1,14 +1,14 @@
 <template>
-  <label v-if="label" :for="uuid">{{ label }}</label>
+  <label v-if="label" :for="id">{{ label }}</label>
   <select
-    :id="uuid"
+    :id="id"
     :value="selectedIdx"
     v-bind="{
       ...$attrs,
       onChange,
     }"
     :aria-invalid="errorMessage ? true : null"
-    :aria-describedby="errorMessage ? `select-error-${uuid}` : null"
+    :aria-describedby="errorMessage ? `select-error-${id}` : null"
   >
     <option disabled value="">Please select one</option>
     <option
@@ -28,57 +28,48 @@
   >
 </template>
 
-<script>
+<script setup>
 import { v4 as uuidv4 } from 'uuid';
 
 import { computed, toRaw } from 'vue';
 
-export default {
-  name: 'HTSelect',
-  props: {
-    label: { type: String, default: '' },
-    options: {
-      type: Array,
-      required: true,
-    },
-    errorMessage: {
-      type: String,
-      default: null,
-    },
-    modelValue: {
-      type: [Object, String, Number, null],
-      required: true,
-    },
+const props = defineProps({
+  label: { type: String, default: '' },
+  options: {
+    type: Array,
+    required: true,
   },
-  emits: ['update:model-value'],
-  setup(props, { emit }) {
-    const uuid = uuidv4();
-
-    const selectedIdx = computed(() => {
-      return props.options.findIndex((option) =>
-        props.valueKey
-          ? option[props.valueKey] === toRaw(props.modelValue)
-          : option === toRaw(props.modelValue),
-      );
-    });
-
-    const onChange = (e) => {
-      const idx = parseInt(e.target.value);
-      emit('update:model-value', props.options[idx]);
-    };
-
-    const parseOptionLabel = (option) => {
-      if (typeof option === 'object' && option?.label) return option.label;
-
-      return option;
-    };
-
-    return {
-      uuid,
-      onChange,
-      parseOptionLabel,
-      selectedIdx,
-    };
+  errorMessage: {
+    type: String,
+    default: null,
   },
+  modelValue: {
+    type: [Object, String, Number, null],
+    required: true,
+  },
+  id: {
+    type: String,
+    default: () => uuidv4(),
+  },
+});
+const emit = defineEmits(['update:model-value']);
+
+const selectedIdx = computed(() => {
+  return props.options.findIndex((option) =>
+    props.valueKey
+      ? option[props.valueKey] === toRaw(props.modelValue)
+      : option === toRaw(props.modelValue),
+  );
+});
+
+const onChange = (e) => {
+  const idx = parseInt(e.target.value);
+  emit('update:model-value', props.options[idx]);
+};
+
+const parseOptionLabel = (option) => {
+  if (typeof option === 'object' && option?.label) return option.label;
+
+  return option;
 };
 </script>
