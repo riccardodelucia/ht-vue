f5f8a84e850ef70cd0ccfb34853af69705596ef8
riccardo.delucia <riccardo.delucia@fht.org>
2025-01-08 17:12:18 +0100

    radiogroup refactor

--- src/components/HTRadioGroup.vue
+++ src/components/HTRadioGroup.vue
@@ -14,6 +14,13 @@
 import { v4 as uuidv4 } from 'uuid';
 import { ref, toRaw, watch } from 'vue';
 
+/*
+ * Vue manages radio groups according to a single modelValue. In addition, its particular internal management via 'value' and 'v-bind' requires the same exact modelValue
+ * to be used for a radio group. This means there is no possibility to split a single radio group in multiple components.
+ * This would imply to use an internal modelValue for each component, to manage the v-model. By having separate modelValues for every radio component,
+ * Vue would not be able to associate multiple radio inputs to the same modelValue, although we are using one single modelValue from the parent component.
+ */
+
 const props = defineProps({
   options: {
     required: true,
